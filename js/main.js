d537fe1130ad9917aa0ae72fde49e4b8471154be
Addie <32201807+runnily@users.noreply.github.com>
2020-07-28 19:56:54 +0100

    Add files via upload
    fixed the menu for the media screen of max width 550

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ function closeMenu() {
     var menu = document.getElementsByClassName("links")[0];
     document.getElementById("arrow-up").style.visibility = "hidden";
     document.getElementById("arrow-down").style.visibility = "visible";
-    var size = 80;
-    minHeight = -20;
+    menu.style.overflow = "hidden";
+    menu.style.paddingTop= "25%";
+    var size = -1;
+    minHeight = -84;
     var id = setInterval(function() {
         if (size < minHeight){
             clearInterval(id);
@@ -11,24 +13,28 @@ function closeMenu() {
             document.getElementById("arrow-up").style.visibility = "visible";
         } else {
             size = size - 1;
-            menu.style.height = size + "vh";
+            menu.style.bottom = size + "%";
         }
     },10);
+
 }
 
 function openMenu() {
     var menu = document.getElementsByClassName("links")[0];
+    menu.style.overflow = "scroll";
     //document.getElementById("nav").style.visibility = "visible";
-    var size = 0;
-    maxHeight = 80;
-    setInterval(function() {
+    var size = -84;
+    maxHeight = -1;
+    menu.style.paddingTop= "10%";
+    var id = setInterval(function() {
         if (size > maxHeight){
+            clearInterval(id);
             document.getElementById("arrow-up").style.visibility = "hidden";
             document.getElementById("arrow-down").style.visibility = "visible";
             
         } else {
             size = size + 1;
-            menu.style.height = size + "vh";
+            menu.style.bottom = size + "%";
         }
     },10);
 }
